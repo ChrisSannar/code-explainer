f4c0503c8f5b8df06788b25facb1e6b802c0ae1a
Christopher Sannar <chris@MACdeMacBook-Pro.local>
2019-01-19 20:06:55 -0700

    Organize file structure and implement better 'replace'
    Started on a better organization and structure of the javascript files to keep things organized. Also fixed the 'replace' function as it was replacing parts of the code that inside the 'tagged' content

---
+++ js/rules.js
@@ -0,0 +1,23 @@
+let rules = [
+    {
+        "tag": "function",
+        "regex": /function/g,
+        "replace": null,
+        "color": "green",
+        "html": "<p>This is a function<p>"
+    },
+    {
+        "tag": "test",
+        "regex": /test/g,
+        "replace": null,
+        "color": "yellow",
+        "html": "<h3>This is a test</h3>"
+    },
+    {
+        "tag": "Parenthesis",
+        "regex": /\(\)/g,
+        "replace": null,
+        "color": "blue",
+        "html": "<h3>Parenthesis</h3>"
+    }
+];
