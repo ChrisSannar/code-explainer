b44cf9451a5f7b451f4677e2f58de853682b595d
Christopher Sannar <chris.sannar.dev@gmail.com>
2019-02-12 16:00:44 -0700

    Some extra stuff to the already working build. Just don't want to loose this if my computer breaks

--- index.js
+++ index.js
@@ -67,7 +67,11 @@ app.get('/get/rules', function(req, res) {
       "token" : "storage.type:function",
       "tokenType" : "storage.type",
       "tokenValue" : "function",
-      "html": "<p>This is a function</p>",
+      "html": `
+        <h2>Function</h2>
+        <p>Functions are bundles of code that can be run on command. They often take input and return output</p>
+        <p>Learn more about functions <a href="">here</a></p>
+      `,
       "links" : ["functionName", "return"]
     },
     {
@@ -83,7 +87,10 @@ app.get('/get/rules', function(req, res) {
       "token" : "punctuation.operator:;",
       "tokenType" : "punctuation.operator",
       "tokenValue" : ";",
-      "html": "<p>This is a semicolon</p>",
+      "html": `
+        <h2>Semicolon</h2>
+        <p>This marks the end of a statment inside a javascript file and divides code into different pieces</p>
+      `,
       "links" : []
     },
     {
@@ -91,7 +98,10 @@ app.get('/get/rules', function(req, res) {
       "token" : "keyword:return",
       "tokenType" : "keyword",
       "tokenValue" : "return",
-      "html": "<p>This is a return statement</p>",
+      "html": `
+        <h2>Return</h2>
+        <p>Placed inside a return statement, 'return' gives output when you call a function.</p>
+      `,
       "links" : ["function"]
     },
     {
@@ -115,7 +125,10 @@ app.get('/get/rules', function(req, res) {
       "token" : "paren.rparen:}",
       "tokenType" : "paren.rparen",
       "tokenValue" : "}",
-      "html": "<p>This is a right curly Brace</p>",
+      "html": `
+        <h2>Curly Brace</h2>
+        <p>These braces cut the code into different blocks of execution. They are usually preceded by a 'function' or 'conditional statement' to specify what code to run after a sepecific event.</p>
+      `,
       "links" : ["leftCurly"]
     },
     {
@@ -123,7 +136,10 @@ app.get('/get/rules', function(req, res) {
       "token" : "paren.lparen:{",
       "tokenType" : "paren.lparen",
       "tokenValue" : "{",
-      "html": "<p>This is a left curly Brace</p>",
+      "html":  `
+        <h2>Curly Brace</h2>
+        <p>These braces cut the code into different blocks of execution. They are usually preceded by a 'function' or 'conditional statement' to specify what code to run after a sepecific event.</p>
+      `,
       "links" : ["rightCurly"]
     },
     {
@@ -141,7 +157,10 @@ app.get('/get/rules', function(req, res) {
       "tag": "functionName",
       "regex" : "function\\s+[a-zA-Z][a-z0-9A-Z]*\\(",
       "tokenType" : "entity.name.function",
-      "html": "<p>This is a function name</p>",
+      "html": `
+        <h2>Function Name</h2>
+        <p>This give a name by which to refer the function</p>
+      `,
       "links" : ["function"]
     },
     {
@@ -155,7 +174,12 @@ app.get('/get/rules', function(req, res) {
       "tag": "number",
       "regex" : "\\d",
       "tokenType" : "constant.numeric",
-      "html": "<p>this is a number</p>",
+      "html": `
+        <h2>Number</h2>
+        <br>
+        <p>Numbers are what you expect: numeric values</p>
+        <p>Anything you can do with 
+      `,
       "links" : []
     },
     {
@@ -164,6 +188,10 @@ app.get('/get/rules', function(req, res) {
       "tokenType" : "comment",
       "html": `
         <h2>Comment</h2>
+        <p>Comments are simply a way of telling the computer to not read this part of the code.</p>
+        <p>Often they are used to describe parts of the code that may be complicated and need an explination, or to summarize what the part of the code does in whole.</p>
+        <p>There are primarily two types of comments. The first being a single line comment which begins with "//" and covers the entire line. The second is a multi-line comment which spans many lines and starts with '/*' and ends with '*/'.</p>
+        <p>You can learn more about comments <a href="#">here</a></p>
       `,
       "links" : []
     }
