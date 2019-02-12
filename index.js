8012131fe4101cac0d09a15b8be4c4a102d3537a
Christopher Sannar <chris.sannar.dev@gmail.com>
2019-02-11 21:31:08 -0700

    Basic functioning app
    Well, it for the most part works. There are a ton of things that we have to do to get it distributed, primarily defining all the possible 'tokens' but the framework is in place.

--- index.js
+++ index.js
@@ -32,37 +32,177 @@ app.get('/', function (req, res) {
 
 app.get('/get/rules', function(req, res) {
   let tokens = req.query.tokens;
-  
+
+  /*
+    Alright, this isn't the best solution, but here we go:
+      Return an object that has the 'type:value' token as a key, but assign
+      each value based upon the regex value inside the database.
+      This won't be very clean, we're gonna have to go through the database
+      for every time to test each regex expression, but at least the frontend
+      will be nice and tidy.
+
+      Also, don't forget about linking to other values (via the regex)
+
+      Or... we could do a duality... where some solid values that will always 
+      be the same (like "function" or "return") have a token-value pairing while
+      others (like "variable-name" or "}") will instead us regex to find their
+      given rule. Let's just split them up into 2 collections:
+        Perminant token pairs: (<type>:<value>) - make such value the key
+        Regex expressions: (/regex/g)
+      If the value isn't found in the token pairs database, then we check each
+      regex till it hits or returns false.
+
+      Just to maybe reitterate (I don't want to read this again), we send back an
+      Object that pairs for each possible value. That means we'll have multiple
+      rules, but that's ok. I'll work out.
+
+      For the regex database, match both the regex and the token type
+  */
+
+
   // *** TEMP
-  let stubbedRules = [
+  let stubTokenRules = [
     {
       "tag": "function",
-      "token": "storage.type:function",
-      "html": "<p>This is a function<p>",
-      "links" : ["return"]
+      "token" : "storage.type:function",
+      "tokenType" : "storage.type",
+      "tokenValue" : "function",
+      "html": "<p>This is a function</p>",
+      "links" : ["functionName", "return"]
+    },
+    {
+      "tag": "let",
+      "token" : "storage.type:let",
+      "tokenType" : "storage.type",
+      "tokenValue" : "let",
+      "html": "<p>This is let</p>",
+      "links" : []
     },
     {
       "tag": "semicolon",
-      "token": "punctuation.operator:;",
-      "html": "<p>This is a semicolon<p>",
+      "token" : "punctuation.operator:;",
+      "tokenType" : "punctuation.operator",
+      "tokenValue" : ";",
+      "html": "<p>This is a semicolon</p>",
       "links" : []
     },
     {
       "tag": "return",
-      "token": "keyword:return",
-      "html": "<p>This is a return statement<p>",
+      "token" : "keyword:return",
+      "tokenType" : "keyword",
+      "tokenValue" : "return",
+      "html": "<p>This is a return statement</p>",
       "links" : ["function"]
     },
+    {
+      "tag": "rightParen",
+      "token" : "paren.rparen:)",
+      "tokenType" : "paren.rparen",
+      "tokenValue" : ")",
+      "html": "<p>This is a right parentheses</p>",
+      "links" : ["leftParen"]
+    },
+    {
+      "tag": "leftParen",
+      "token" : "paren.lparen:(",
+      "tokenType" : "paren.lparen",
+      "tokenValue" : "(",
+      "html": "<p>This is a left parentheses</p>",
+      "links" : ["rightParen"]
+    },
+    {
+      "tag": "rightCurly",
+      "token" : "paren.rparen:}",
+      "tokenType" : "paren.rparen",
+      "tokenValue" : "}",
+      "html": "<p>This is a right curly Brace</p>",
+      "links" : ["leftCurly"]
+    },
+    {
+      "tag": "leftCurly",
+      "token" : "paren.lparen:{",
+      "tokenType" : "paren.lparen",
+      "tokenValue" : "{",
+      "html": "<p>This is a left curly Brace</p>",
+      "links" : ["rightCurly"]
+    },
+    {
+      "tag": "assignment",
+      "token" : "keyword.operator:=",
+      "tokenType" : "keyword.operator",
+      "tokenValue" : "=",
+      "html": "<p>This is an assignment operator</p>",
+      "links" : ["variable"]
+    },
   ];
+  
+  let stubRegexRules = [
+    {
+      "tag": "functionName",
+      "regex" : "function\\s+[a-zA-Z][a-z0-9A-Z]*\\(",
+      "tokenType" : "entity.name.function",
+      "html": "<p>This is a function name</p>",
+      "links" : ["function"]
+    },
+    {
+      "tag": "variable",
+      "regex" : ".*",
+      "tokenType" : "identifier",
+      "html": "<p>This is a variable name</p>",
+      "links" : ["let"]
+    },
+    {
+      "tag": "number",
+      "regex" : "\\d",
+      "tokenType" : "constant.numeric",
+      "html": "<p>this is a number</p>",
+      "links" : []
+    },
+    {
+      "tag": "comment",
+      "regex" : "",//"\/\/.*",
+      "tokenType" : "comment",
+      "html": `
+        <h2>Comment</h2>
+      `,
+      "links" : []
+    }
+  ];
+
+  let rules = {};
+
+  // parse each of the tokens recieved
+  if (tokens){
+    tokens.forEach(token => {
+      let tokenSig = `${token.type}:${token.value}`;
+      let found = stubTokenRules.find(val => val.token == tokenSig);
+      if (found) {
+        rules[tokenSig] = found;
+      } else {
+        
+        found = stubRegexRules.find(val => {
+          if (!val.regex){
+            val.tokenType == token.type;
+          }
+          let regy = new RegExp(val.regex, 'g');
+          return regy.test(token.line) && val.tokenType == token.type;
+        });
+        if (found) {
+          rules[tokenSig] = found;
+        } else {
+          console.log("Token not found: ", token);
+        }
+      }
+    });
+  }
   // ***
 
-  res.status(200).send(JSON.stringify(stubbedRules));
+  res.status(200).send(JSON.stringify(rules));
 });
 
 app.get('*', function(req, res) {
   res.status(404).sendFile(`${__dirname}/web/404.html`);
 });
-
 
 //start the server
 if (!process.env.PORT) { process.env.PORT = 8082 }
