5dfe3647efa0261cd1beadd419f3f9be6dda0caf
Christopher Sannar <chris.sannar.dev@gmail.com>
2020-03-24 16:48:03 -0600

    Added environment variables and started to shape the app more like an api

--- pages.js
+++ pages.js
@@ -5,7 +5,7 @@ var passwordHash = require('password-hash');
 
 // Serves all the pages for the html file
 router.get('/', function (req, res) {
-  if (/mobile/i.test(req.headers['user-agent'])){
+  if (/mobile/i.test(req.headers['user-agent'])) {
     res.status(200).sendFile(`${__dirname}/web/html/mobile.html`);
   } else {
     res.status(200).sendFile(`${__dirname}/web/html/index.html`);
@@ -22,7 +22,7 @@ router.get('/stats', async function (req, res) {
   try {
     let stats = await mongodb.collection("javascriptStats").find({}).toArray();
     // console.log(stats);
-    
+
     // res.status(200).json(stats);
     res.status(200).sendFile(`${__dirname}/web/html/stats.html`);
   } catch (e) {
@@ -32,18 +32,18 @@ router.get('/stats', async function (req, res) {
 
 let pass = false;
 let hash = 'sha1$69a6f84c$1$c1a80e1cadeb6bc4dd1fdb74577d82bbd4f4476e';
-router.post('/password', function(req, res) {
+router.post('/password', function (req, res) {
   let password = req.body.password;
   if (passwordHash.verify(password, hash)) {
     pass = true;
     res.status(200).send({ status: `SUCCESS` });
   } else {
     pass = false;
-    res.status(200).send({ status: `FAIL`});
+    res.status(200).send({ status: `FAIL` });
   }
 });
 
-router.get('/admin', function(req, res) {
+router.get('/admin', function (req, res) {
   if (pass) {
     res.status(200).sendFile(`${__dirname}/web/html/admin.html`);
   } else {
@@ -51,7 +51,7 @@ router.get('/admin', function(req, res) {
   }
 });
 
-router.get('*', function(req, res) {
+router.get('*', function (req, res) {
   res.status(404).sendFile(`${__dirname}/web/html/404.html`);
 });
 
