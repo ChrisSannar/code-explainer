620a4c022f808ffcef216e3037d80be5cb0b17c1
Christopher Sannar <chris.sannar.dev@gmail.com>
2020-03-27 00:20:18 -0600

    Set up the whole api. Need to code it now

--- app/api.js
+++ app/api.js
@@ -12,7 +12,7 @@ const client = new MongoClient(mongoURL, { useNewUrlParser: true, useUnifiedTopo
 client.connect(async function (err, client) {
   if (err) { throw err; }
   mongodb = client.db("code-explainer");
-  console.log("MongoDB connected");
+  console.log("MongoDB connected api");
 });
 
 // *** TESTING
@@ -22,7 +22,7 @@ router.get('/', function (req, res) {
 // ***
 
 // GET all the rules of a particular language
-router.get('/all/:lang', async function (req, res, next) {
+router.get('/:lang', async function (req, res, next) {
   // req query token: {"type":"storage.type","value":"let","line":"let x = 0; "}
   if (mongodb) {
 
@@ -40,28 +40,14 @@ router.get('/all/:lang', async function (req, res, next) {
 });
 
 // GET all the tokenized rules of the given language
-router.get('/all/token/:lang', async function (req, res, next) {
+router.get('/token/:lang', async function (req, res, next) {
   if (mongodb) {
 
     // Pull the language and get the database call
     let lang = req.params.lang;
     let tokens = await mongodb.collection(`${lang}TokenRules`).find({}).toArray();
 
-    // Send back the data
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(tokens);
-  } else {
-    next('Database not set');
-  }
-});
-
-// GET the tokenized rules of the language given a list of tokens
-router.get('/all/token/:lang', async function (req, res, next) {
-  if (mongodb) {
-
-    // Pull the language and get the database call
-    let lang = req.params.lang;
-    let tokens = await mongodb.collection(`${lang}TokenRules`).find({}).toArray();
+    // *** TODO: allow a query to filter the results
 
     // Send back the data
     res.setHeader('Content-Type', 'application/json');
@@ -72,18 +58,95 @@ router.get('/all/token/:lang', async function (req, res, next) {
 });
 
 // GET the tokenized rules of the given language
-router.get('/all/regex/:lang', async function (req, res, next) {
+router.get('/regex/:lang', async function (req, res, next) {
   if (mongodb) {
 
     // Pull the language and get the database call
     let lang = req.params.lang;
     let regex = await mongodb.collection(`${lang}RegexRules`).find({}).toArray();
 
+    // *** TODO: allow a query to filter the results
+
     // Send back the data
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(regex);
   } else {
     next('Database not set');
+  }
+});
+
+// PUT update a token rule
+router.put('/token/:lang/:id', async function (req, res, next) {
+  if (mongodb) {
+
+    let lang = req.params.lang;
+    let id = req.params.id;
+
+    res.status(201).send(`OK`);
+  } else {
+    next('Database not set');
+  }
+});
+
+// PUT update a regex rule
+router.put('/regex/:lang/:id', async function (req, res, next) {
+  if (mongodb) {
+
+    let lang = req.params.lang;
+    let id = req.params.id;
+    let updatedRule = req.body;
+
+    res.status(201).send(`OK`);
+  } else {
+    next('Database not set');
+  }
+});
+
+// POST a new roken rule
+router.post('/token/:lang', async function (req, res, next) {
+  if (mongodb) {
+
+    let lang = req.params.lang;
+    let newRule = req.body;
+
+    res.status(201).send(`OK`);
+  } else {
+    next('Database not set');
+  }
+});
+
+// POST a new roken rule
+router.post('/regex/:lang', async function (req, res, next) {
+  if (mongodb) {
+
+    let lang = req.params.lang;
+    let newRule = req.body;
+
+    res.status(201).send(`OK`);
+  } else {
+    next('Database not set');
+  }
+});
+
+// DELETE a token rule given an id
+router.delete('/token/:lang/:id', async function (req, res, next) {
+  if (mongodb) {
+
+    let lang = req.params.lang;
+    let id = req.params.id;
+
+    res.status(200).send(`OK`);
+  }
+});
+
+// DELETE a regex rule given an id
+router.delete('/regex/:lang/:id', async function (req, res, next) {
+  if (mongodb) {
+
+    let lang = req.params.lang;
+    let id = req.params.id;
+
+    res.status(200).send(`OK`);
   }
 });
 
