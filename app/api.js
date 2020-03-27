e000300821f97526f100505d521e4d078c1be7cf
Christopher Sannar <chris.sannar.dev@gmail.com>
2020-03-26 23:57:35 -0600

    Added basic GET for the api

--- app/api.js
+++ app/api.js
@@ -15,15 +15,37 @@ client.connect(async function (err, client) {
   console.log("MongoDB connected");
 });
 
-// Returns all the rules of a particular language
-router.get('/rules/all/:lang', async function (req, res, next) {
-  console.log(`MARK`);
+// *** TESTING
+router.get('/', function (req, res) {
+  res.json({ message: 'MARK' });
+})
+// ***
+
+// GET all the rules of a particular language
+router.get('/all/:lang', async function (req, res, next) {
   // req query token: {"type":"storage.type","value":"let","line":"let x = 0; "}
   if (mongodb) {
 
     // Pull the language and get the database call
     let lang = req.params.lang;
-    let tokens = await mongodb.collection(lang + "TokenRules").find({}).toArray();
+    let tokens = await mongodb.collection(`${lang}TokenRules`).find({}).toArray();
+    let regex = await mongodb.collection(`${lang}RegexRules`).find({}).toArray();
+
+    // Send back the data
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(tokens.concat(regex));
+  } else {
+    next('Database not set');
+  }
+});
+
+// GET all the tokenized rules of the given language
+router.get('/all/token/:lang', async function (req, res, next) {
+  if (mongodb) {
+
+    // Pull the language and get the database call
+    let lang = req.params.lang;
+    let tokens = await mongodb.collection(`${lang}TokenRules`).find({}).toArray();
 
     // Send back the data
     res.setHeader('Content-Type', 'application/json');
@@ -33,9 +55,36 @@ router.get('/rules/all/:lang', async function (req, res, next) {
   }
 });
 
-// Gets the rules given a query of each one
-router.get('/rules/:lang', async function (req, res, next) {
+// GET the tokenized rules of the language given a list of tokens
+router.get('/all/token/:lang', async function (req, res, next) {
+  if (mongodb) {
 
-})
+    // Pull the language and get the database call
+    let lang = req.params.lang;
+    let tokens = await mongodb.collection(`${lang}TokenRules`).find({}).toArray();
+
+    // Send back the data
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(tokens);
+  } else {
+    next('Database not set');
+  }
+});
+
+// GET the tokenized rules of the given language
+router.get('/all/regex/:lang', async function (req, res, next) {
+  if (mongodb) {
+
+    // Pull the language and get the database call
+    let lang = req.params.lang;
+    let regex = await mongodb.collection(`${lang}RegexRules`).find({}).toArray();
+
+    // Send back the data
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(regex);
+  } else {
+    next('Database not set');
+  }
+});
 
 module.exports = router;
