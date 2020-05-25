09e00df01664afbf6d25f5bbd1d38c2f9ac8702c
Christopher Sannar <chris.sannar.dev@gmail.com>
2020-05-24 21:10:28 -0600

    Final Stylings
    I decided the donation button would be for another time.

--- public/js/display.js
+++ public/js/display.js
@@ -1,4 +1,4 @@
-// set the display height to change when the window changes
+// Set the display height to change when the window changes
 window.onresize = changeDisplayHeight;
 function changeDisplayHeight() {
   document.querySelector("#display").style.height =
@@ -13,9 +13,16 @@ function newTabLinks(el) {
   });
 }
 
-// uses 'highlighjs' to highlight code portions of the document
+// Uses 'highlighjs' to highlight code portions of the document
 function highlightCode() {
   document.querySelectorAll("pre code").forEach((block) => {
     hljs.highlightBlock(block);
   });
+}
+
+// Displays the settings dropdown
+let settingsToggle = true;
+function toggleSettings() {
+  settingsToggle = !settingsToggle;
+  document.querySelector('#settings').style.display = settingsToggle ? 'block' : 'none';
 }
